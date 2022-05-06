56c06912261d477357021c2c92c81c16004942d0
luis <luisfgp80@gmail.com>
2022-05-06 17:54:22 -0400

    customize basic view is ready

--- client/src/views/Customize.js
+++ client/src/views/Customize.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TopNav from "../components/TopNav";
 import BottomBar from "../components/BottomBar";
+import {Link} from "react-router-dom";
 
 const Customize = () => {
     return (
@@ -8,7 +9,44 @@ const Customize = () => {
             <div>
                 <TopNav/>
             </div>
-            <h1>Customize</h1>
+            <div className={"bg-sky-900"}>
+                <div className=" pt-9 text-center">
+                    <h1 className="text-2xl font-Oswald text-yellow-300 pl-4">Let's start with the wood</h1>
+                </div>
+                <div className="flex justify-center items-center my-12">
+                    <div className={"w-18 h-60 bg-white opacity-10 mx-6"}>
+                        arrow left
+                    </div>
+                    <img
+                        className=" opacity-25"
+                        src={require("../images/Fence_10.png")}
+                        alt="Fence Main"
+                        style={{width:"16%"}}/>
+                    <img
+                        className=""
+                        src={require("../images/Fence_10.png")}
+                        alt="Fence Main"
+                        style={{width:"30%"}}/>
+                    <img
+                        className=" opacity-25"
+                        src={require("../images/Fence_10.png")}
+                        alt="Fence Main"
+                        style={{width:"16%"}}/>
+                    <div className={"w-18 h-60 bg-white opacity-10 mx-6"}>
+                        arrow right
+                    </div>
+
+                </div>
+                <div className={"text-white font-sans text-center pb-6"}>
+                    <h3>Wood description from DB</h3>
+                </div>
+                <div className={"text-center pb-8"}>
+                    <button
+                        className={"h-8 w-32 bg-fuchsia-500 hover:bg-sky-300 text-white text-l font-bold rounded"}>
+                        <Link to={"/customize"}>Select this</Link>
+                    </button>
+                </div>
+            </div>
             <div>
                 <BottomBar/>
             </div>
